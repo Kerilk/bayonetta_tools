47697c5ed6514cd64a4def6c4e0f4235892699e7
Brice Videau <brice.videau@imag.fr>
2017-10-28 19:55:01 +0200

    Added a converter from WiiU Bayo to PC Bayo.

---
+++ wmb_convert_wiiu_pc.rb
@@ -0,0 +1,10 @@
+require_relative 'lib/bayonetta.rb'
+include Bayonetta
+
+
+input_file = ARGV[0]
+
+raise "Invalid file #{input_file}" unless File::file?(input_file)
+Dir.mkdir("wmb_output") unless Dir.exist?("wmb_output")
+fl = WMBFile::convert(input_file, "wmb_output/"+File.basename(input_file))
+
