81713071b403258563f2a7c0ee91ce874424f7f4
Brice Videau <brice.videau@free.fr>
2018-06-27 02:57:02 +0200

    Missing values will be ignored.

--- convert_noe_pose.rb
+++ convert_noe_pose.rb
@@ -3,12 +3,12 @@ require 'yaml'
 h = YAML::load_file(ARGV[0])
 
 h.each { |k, v|
-  x = -v[5] * Math::PI/180.0
-  y = v[3] * Math::PI/180.0
-  z = -v[4] * Math::PI/180.0
-  v[3] = x
-  v[4] = y
-  v[5] = z
+  x = -v[5] * Math::PI/180.0 if v[5]
+  y = v[3] * Math::PI/180.0 if v[3]
+  z = -v[4] * Math::PI/180.0 if v[4]
+  v[3] = x if v[5]
+  v[4] = y if v[3]
+  v[5] = z if v[4]
 }
 
 
