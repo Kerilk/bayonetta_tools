9836d6b21e3c8741721b0041b7e6a6692d6873d2
Brice Videau <brice.videau@free.fr>
2021-10-31 19:07:15 -0500

    Exp should reuse part of Mot.

--- lib/bayonetta.rb
+++ lib/bayonetta.rb
@@ -9,13 +9,13 @@ require_relative 'bayonetta/eff'
 require_relative 'bayonetta/wtb'
 require_relative 'bayonetta/wmb'
 require_relative 'bayonetta/wmb3'
-require_relative 'bayonetta/exp'
 require_relative 'bayonetta/bxm'
 require_relative 'bayonetta/clp'
 require_relative 'bayonetta/clh'
 require_relative 'bayonetta/clw'
 require_relative 'bayonetta/scr'
 require_relative 'bayonetta/mot'
+require_relative 'bayonetta/exp'
 
 module Bayonetta
   include Alignment
